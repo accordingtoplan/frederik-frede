c4757a9db35d60c28881df41b449aceebc4946f4
According To Plan <frederikfrede@gmail.com>
2026-06-11 15:17:25 +0200

    Shorten cookie consent text

--- footer.js
+++ footer.js
@@ -33,7 +33,7 @@
       </div>
     </div>
     <div class="cookie-consent" id="cookieConsent">
-      <span class="cookie-text">This site uses analytics.</span>
+      <span class="cookie-text">Uses analytics.</span>
       <div class="cookie-actions">
         <button id="cookieDecline">Decline</button>
         <button id="cookieAccept">Accept</button>
